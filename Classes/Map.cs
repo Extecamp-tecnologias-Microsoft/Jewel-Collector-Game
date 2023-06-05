da4ac69bcfd3c186ec5e3a20f2332edf8a43c666
Gabriel Rodrigues <gabers357@gmail.com>
2023-06-04 22:44:48 -0300

    pequenas correcoes de texto
    Co-authored-by: Eduardo Franco <eduardoofraanco@gmail.com>

--- Classes/Map.cs
+++ Classes/Map.cs
@@ -4,88 +4,95 @@ namespace JewellNS;
 /// </summary>
 public class Map
 {
-    private Cell[,] arrayObj;
-    private int linha;
-    private int coluna;
+  private Cell[,] arrayObj;
+  private int line;
+  private int column;
 
-    public int getNumberOfColunas() { return this.coluna; }
-    public int getNumberOfLinhas() { return this.linha; }
-    public Map() { }
-    public Map(int width, int height)
-    {
-        this.linha = width;
-        this.coluna = height;
-        arrayObj = new Cell[width, height];
-    }
-    /// <summary>
-    /// getCell é responsavel por retornar o objeto que é contido através dos parametros enviados
-    /// </summary>
-    /// <param name="linha"> variavel que determina a linha da Cell</param>
-    /// <param name="coluna"> variavel que determina a coluna da Cell</param>
-    /// <returns> retorna o objeto da Cell</returns>
-    public Cell getCell(int linha, int coluna)
-    {
-        return arrayObj[linha, coluna];
-    }
-    public void setCell(int i, int j, Cell obj)
-    {
-        if (obj is Jewell jewell)
-        {
-            arrayObj[i, j] = jewell;
-        }
-        else if (obj is Obstacle obstacle)
-        {
-            arrayObj[i, j] = obstacle;
-        }
-        else if (obj is Robot Robot)
-        {
-            arrayObj[i, j] = Robot;
-        }
-    }
-    public void removeCell(int i, int j)
-    {
-        arrayObj[i, j] = null;
-    }
-    public void PrintMap()
-    {
-        Console.Clear();
 
-        for (int i = 0; i < linha; i++)
-        {
-            for (int j = 0; j < coluna; j++)
-            {
-                if (arrayObj[i, j] is null)
-                {
-                    Console.Write(" -- ");
-                }
-                else if (arrayObj[i, j] is Jewell jewell)
-                {
-                    Console.Write(jewell.Symbol);
-                }
-                else if (arrayObj[i, j] is Obstacle obstacle)
-                {
-                    Console.Write(obstacle.Symbol);
-                }
-                else if (arrayObj[i, j] is Robot Robot)
-                {
-                    Console.Write(Robot.Symbol);
-                }
-            }
-            Console.WriteLine();
-        }
-    }
-    public void FindRobotPosition()
+  public int getColumnsNumbers() { return this.column; }
+  public int getLinesNumbers() { return this.line; }
+  public Map() { }
+  /// <summary>
+  /// getCell é responsavel por retornar o objeto que é contido através dos parametros enviados
+  /// </summary>
+  /// <param name="linha"> variavel que determina a linha da Cell</param>
+  /// <param name="coluna"> variavel que determina a coluna da Cell</param>
+  /// <returns> retorna o objeto da Cell</returns>
+  public Map(int width, int height)
+  {
+    this.line = width;
+    this.column = height;
+    arrayObj = new Cell[width, height];
+  }
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <param name="line"></param>
+  /// <param name="column"></param>
+  /// <returns></returns>
+  public Cell getCell(int line, int column)
+  {
+    return arrayObj[line, column];
+  }
+  public void setCell(int i, int j, Cell obj)
+  {
+    if (obj is Jewell jewell)
     {
-        for (int i = 0; i < linha; i++)
-        {
-            for (int j = 0; j < coluna; j++)
-            {
-                if (arrayObj[i, j] is Robot Robot)
-                {
-                    Robot.setRobotLine(i);
-                    Robot.setRobotColumn(j);
-                }
-            }
-        }
+      arrayObj[i, j] = jewell;
     }
+    else if (obj is Obstacle obstacle)
+    {
+      arrayObj[i, j] = obstacle;
+    }
+    else if (obj is Robot Robot)
+    {
+      arrayObj[i, j] = Robot;
+    }
+  }
+  public void removeCell(int i, int j)
+  {
+    arrayObj[i, j] = null;
+  }
+  public void PrintMap()
+  {
+    Console.Clear();
+
+    for (int i = 0; i < line; i++)
+    {
+      for (int j = 0; j < column; j++)
+      {
+        if (arrayObj[i, j] is null)
+        {
+          Console.Write(" -- ");
+        }
+        else if (arrayObj[i, j] is Jewell jewell)
+        {
+          Console.Write(jewell.Symbol);
+        }
+        else if (arrayObj[i, j] is Obstacle obstacle)
+        {
+          Console.Write(obstacle.Symbol);
+        }
+        else if (arrayObj[i, j] is Robot Robot)
+        {
+          Console.Write(Robot.Symbol);
+        }
+      }
+      Console.WriteLine();
+    }
+  }
+  public void FindRobotPosition()
+  {
+    for (int i = 0; i < line; i++)
+    {
+      for (int j = 0; j < column; j++)
+      {
+        if (arrayObj[i, j] is Robot Robot)
+        {
+          Robot.setRobotLine(i);
+          Robot.setRobotColumn(j);
+        }
+      }
+    }
+  }
 }
