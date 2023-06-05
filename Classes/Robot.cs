478a7776166909ed678eef178443e58211423ee8
Gabriel Rodrigues <gabers357@gmail.com>
2023-06-04 23:03:21 -0300

    Pequenas correções de texto & doc #12 Co-authored-by: Eduardo Franco <eduardoofraanco@gmail.com>

--- Classes/Robot.cs
+++ Classes/Robot.cs
@@ -1,126 +1,129 @@
 namespace JewellNS;
 public class Robot : Cell
 {
-    private int bagValue = 0;
-    private Cell[] bagItems = new Cell[] { };
-    private int RobotLine;
-    private int RobotColumn;
-    public int getRobotLine() { return this.RobotLine; }
-    public int getRobotColumn() { return this.RobotColumn; }
-    public void setRobotLine(int linha) { this.RobotLine = linha; }
-    public void setRobotColumn(int coluna) { this.RobotColumn = coluna; }
+  private int bagValue = 0;
+  private Cell[] bagItems = new Cell[] { };
+  private int RobotLine;
+  private int RobotColumn;
+  public int getRobotLine() { return this.RobotLine; }
+  public int getRobotColumn() { return this.RobotColumn; }
+  public void setRobotLine(int line) { this.RobotLine = line; }
+  public void setRobotColumn(int column) { this.RobotColumn = column; }
 
-    public string toString() { return $"Bag total items: {this.bagItems.Length} | Bag total value: {this.bagValue} | Energy: {this.LevelEnergy}"; }
-    public void verifyEnergyLevel()
+  public string toString() { return $"Bag total items: {this.bagItems.Length} | Bag total value: {this.bagValue} | Energy: {this.LevelEnergy}"; }
+  public void verifyEnergyLevel()
+  {
+    Console.Clear();
+    if (LevelEnergy <= 0)
     {
-        Console.Clear();
-        if (LevelEnergy <= 0)
-        {
-            Console.WriteLine("Suas energias acabaram");
-            Environment.Exit(0);
-        }
+      Console.WriteLine("Suas energias acabaram");
+      Environment.Exit(0);
     }
-    public void moveToLeft(Map map)
+  }
+  public void moveToLeft(Map map)
+  {
+    if (this.getRobotColumn() > 0)
     {
-        if (this.getRobotColumn() > 0)
-        {
-            if (map.getCell(getRobotLine(), getRobotColumn() - 1) is not Jewell or Obstacle)
-            {
-                LevelEnergy--;
-                map.removeCell(getRobotLine(), getRobotColumn());
-                map.setCell(getRobotLine(), getRobotColumn() - 1, this);
-                verifyEnergyLevel();
-            }
-        }
+      if (map.getCell(getRobotLine(), getRobotColumn() - 1) is not Jewell or Obstacle)
+      {
+        LevelEnergy--;
+        map.removeCell(getRobotLine(), getRobotColumn());
+        map.setCell(getRobotLine(), getRobotColumn() - 1, this);
+        verifyEnergyLevel();
+      }
     }
-    public void moveToRight(Map map)
+  }
+  public void moveToRight(Map map)
+  {
+    if (this.getRobotColumn() <= map.getColumnsNumbers())
     {
-        if (this.getRobotColumn() <= map.getNumberOfColunas())
-        {
-            if (map.getCell(getRobotLine(), getRobotColumn() + 1) is not Jewell or Obstacle)
-            {
-                LevelEnergy--;
-                map.removeCell(getRobotLine(), getRobotColumn());
-                map.setCell(getRobotLine(), getRobotColumn() + 1, this);
-                verifyEnergyLevel();
-            }
-        }
+      if (map.getCell(getRobotLine(), getRobotColumn() + 1) is not Jewell or Obstacle)
+      {
+        LevelEnergy--;
+        map.removeCell(getRobotLine(), getRobotColumn());
+        map.setCell(getRobotLine(), getRobotColumn() + 1, this);
+        verifyEnergyLevel();
+      }
     }
-    public void moveToTop(Map map)
+  }
+  public void moveToTop(Map map)
+  {
+    if (this.getRobotLine() > 0)
     {
-        if (this.getRobotLine() > 0)
-        {
-            if (map.getCell(getRobotLine() - 1, getRobotColumn()) is not Jewell or Obstacle)
-            {
-                LevelEnergy--;
-                map.removeCell(getRobotLine(), getRobotColumn());
-                map.setCell(getRobotLine() - 1, getRobotColumn(), this);
-                verifyEnergyLevel();
-            }
-        }
+      if (map.getCell(getRobotLine() - 1, getRobotColumn()) is not Jewell or Obstacle)
+      {
+        LevelEnergy--;
+        map.removeCell(getRobotLine(), getRobotColumn());
+        map.setCell(getRobotLine() - 1, getRobotColumn(), this);
+        verifyEnergyLevel();
+      }
     }
-    public void moveToBottom(Map map)
+  }
+  public void moveToBottom(Map map)
+  {
+    if (this.getRobotLine() <= map.getLinesNumbers())
     {
-        if (this.getRobotLine() <= map.getNumberOfLinhas())
-        {
-            if (map.getCell(getRobotLine() + 1, getRobotColumn()) is not Jewell or Obstacle)
-            {
-                LevelEnergy--;
-                map.removeCell(getRobotLine(), getRobotColumn());
-                map.setCell(getRobotLine() + 1, getRobotColumn(), this);
-                verifyEnergyLevel();
-            }
-        }
+      if (map.getCell(getRobotLine() + 1, getRobotColumn()) is not Jewell or Obstacle)
+      {
+        LevelEnergy--;
+        map.removeCell(getRobotLine(), getRobotColumn());
+        map.setCell(getRobotLine() + 1, getRobotColumn(), this);
+        verifyEnergyLevel();
+      }
     }
+  }
 
-    public void captureItem(Map map)
-    {
-        int linhaDoJogador = getRobotLine();
-        int colunaDoJogador = getRobotColumn();
-        int captureRange = 1;
+  public void captureItem(Map map)
+  {
+    int playerLine = getRobotLine();
+    int columnPlayer = getRobotColumn();
+    int captureRange = 1;
 
-        if ((map.getCell(linhaDoJogador + captureRange, colunaDoJogador) is Jewell) ||
-            (map.getCell(linhaDoJogador + captureRange, colunaDoJogador) is Obstacle obstacle3) && obstacle3.Symbol == " $$ ")
-        {
-            updateEnergy(map.getCell(linhaDoJogador + captureRange, colunaDoJogador));
-            updateBag(map.getCell(linhaDoJogador + captureRange, colunaDoJogador));
-            map.removeCell(linhaDoJogador + captureRange, colunaDoJogador);
-        }
-        else if ((map.getCell(linhaDoJogador - captureRange, colunaDoJogador) is Jewell) ||
-        (map.getCell(linhaDoJogador - captureRange, colunaDoJogador) is Obstacle obstacle2) && obstacle2.Symbol == " $$ ")
-        {
-            updateEnergy(map.getCell(linhaDoJogador - captureRange, colunaDoJogador));
-            updateBag(map.getCell(linhaDoJogador - captureRange, colunaDoJogador));
-            map.removeCell(linhaDoJogador - captureRange, colunaDoJogador);
-        }
-        else if ((map.getCell(linhaDoJogador, colunaDoJogador + captureRange) is Jewell) ||
-        (map.getCell(linhaDoJogador, colunaDoJogador + captureRange) is Obstacle obstacle1) && obstacle1.Symbol == " $$ ")
-        {
-            updateEnergy(map.getCell(linhaDoJogador, colunaDoJogador + captureRange));
-            updateBag(map.getCell(linhaDoJogador, colunaDoJogador + captureRange));
-            map.removeCell(linhaDoJogador, colunaDoJogador + captureRange);
-        }
-        else if ((map.getCell(linhaDoJogador, colunaDoJogador - captureRange) is Jewell) ||
-        (map.getCell(linhaDoJogador, colunaDoJogador - captureRange) is Obstacle obstacle) && obstacle.Symbol == " $$ ")
-        {
-            updateEnergy(map.getCell(linhaDoJogador, colunaDoJogador - captureRange));
-            updateBag(map.getCell(linhaDoJogador, colunaDoJogador - captureRange));
-            map.removeCell(linhaDoJogador, colunaDoJogador - captureRange);
-        }
-    }
-
-    private void updateBag(Object objeto)
+    if ((map.getCell(playerLine + captureRange, columnPlayer) is Jewell) ||
+        (map.getCell(playerLine + captureRange, columnPlayer) is Obstacle obstacle3) && obstacle3.Symbol == " $$ ")
     {
-        if (objeto is Jewell jewell)
-        {
-            this.bagValue = this.bagValue + jewell.Point;
-            Array.Resize(ref bagItems, bagItems.Length + 1);
-            bagItems[bagItems.Length - 1] = jewell;
-        }
+      updateEnergy(map.getCell(playerLine + captureRange, columnPlayer));
+      updateBag(map.getCell(playerLine + captureRange, columnPlayer));
+      map.removeCell(playerLine + captureRange, columnPlayer);
     }
-
-    private void updateEnergy(Cell objeto)
+    else if ((map.getCell(playerLine - captureRange, columnPlayer) is Jewell) ||
+    (map.getCell(playerLine - captureRange, columnPlayer) is Obstacle obstacle2) && obstacle2.Symbol == " $$ ")
     {
-        LevelEnergy = LevelEnergy + objeto.LevelEnergy;
+      updateEnergy(map.getCell(playerLine - captureRange, columnPlayer));
+      updateBag(map.getCell(playerLine - captureRange, columnPlayer));
+      map.removeCell(playerLine - captureRange, columnPlayer);
     }
+    else if ((map.getCell(playerLine, columnPlayer + captureRange) is Jewell) ||
+    (map.getCell(playerLine, columnPlayer + captureRange) is Obstacle obstacle1) && obstacle1.Symbol == " $$ ")
+    {
+      updateEnergy(map.getCell(playerLine, columnPlayer + captureRange));
+      updateBag(map.getCell(playerLine, columnPlayer + captureRange));
+      map.removeCell(playerLine, columnPlayer + captureRange);
+    }
+    else if ((map.getCell(playerLine, columnPlayer - captureRange) is Jewell) ||
+    (map.getCell(playerLine, columnPlayer - captureRange) is Obstacle obstacle) && obstacle.Symbol == " $$ ")
+    {
+      updateEnergy(map.getCell(playerLine, columnPlayer - captureRange));
+      updateBag(map.getCell(playerLine, columnPlayer - captureRange));
+      map.removeCell(playerLine, columnPlayer - captureRange);
+    }
+  }
+  /// <summary>
+  /// Atualiza a quantidade de itens na bolsa e o valor da mesma 
+  /// </summary>
+  /// <param name="cellObject">Celula atual do jogador</param>
+  private void updateBag(Object cellObject)
+  {
+    if (cellObject is Jewell jewell)
+    {
+      this.bagValue = this.bagValue + jewell.Point;
+      Array.Resize(ref bagItems, bagItems.Length + 1);
+      bagItems[bagItems.Length - 1] = jewell;
+    }
+  }
+
+  private void updateEnergy(Cell cellObject)
+  {
+    LevelEnergy = LevelEnergy + cellObject.LevelEnergy;
+  }
 }
--- JewellCollector.cs
+++ JewellCollector.cs
@@ -1,117 +1,118 @@
 ﻿namespace JewellNS;
 public class JewelCollector
 {
-    public static void Main(string[] args)
-    {
-        Map map = new Map(10, 10);
-        Jewell jr = new Jewell { Name = "red", Symbol = " JR ", Point = 100, LevelEnergy = 0 };
-        Jewell jg = new Jewell { Name = "green", Symbol = " JG ", Point = 50, LevelEnergy = 0 };
-        Jewell jb = new Jewell { Name = "blue", Symbol = " JB ", Point = 10, LevelEnergy = 5 };
-        Obstacle water = new Obstacle { Name = "water", Symbol = " ## ", LevelEnergy = 0 };
-        Obstacle tree = new Obstacle { Name = "tree", Symbol = " $$ ", LevelEnergy = 3 };
-        Robot Robot = new Robot { Name = "robot", Symbol = " ME ", Point = 0, LevelEnergy = 5 };
-        map.setCell(0, 0, Robot);
-        map.setCell(1, 9, jr);
-        map.setCell(8, 8, jr);
-        map.setCell(9, 1, jg);
-        map.setCell(7, 6, jg);
-        map.setCell(3, 4, jb);
-        map.setCell(2, 1, jb);
-        map.setCell(5, 0, water);
-        map.setCell(5, 1, water);
-        map.setCell(5, 2, water);
-        map.setCell(5, 3, water);
-        map.setCell(5, 4, water);
-        map.setCell(5, 5, water);
-        map.setCell(5, 6, water);
-        map.setCell(5, 9, tree);
-        map.setCell(3, 9, tree);
-        map.setCell(8, 3, tree);
-        map.setCell(2, 5, tree);
-        map.setCell(1, 4, tree);
-        map.PrintMap();
-        map.FindRobotPosition();
-        Play(Robot, tree, water, jr, jb, jg, map);
-    }
+  public static void Main(string[] args)
+  {
+    Map map = new Map(10, 10);
+    Jewell jr = new Jewell { Name = "red", Symbol = " JR ", Point = 100, LevelEnergy = 0 };
+    Jewell jg = new Jewell { Name = "green", Symbol = " JG ", Point = 50, LevelEnergy = 0 };
+    Jewell jb = new Jewell { Name = "blue", Symbol = " JB ", Point = 10, LevelEnergy = 5 };
+    Obstacle water = new Obstacle { Name = "water", Symbol = " ## ", LevelEnergy = 0 };
+    Obstacle tree = new Obstacle { Name = "tree", Symbol = " $$ ", LevelEnergy = 3 };
+    Robot Robot = new Robot { Name = "robot", Symbol = " ME ", Point = 0, LevelEnergy = 5 };
+    map.setCell(0, 0, Robot);
+    map.setCell(1, 9, jr);
+    map.setCell(8, 8, jr);
+    map.setCell(9, 1, jg);
+    map.setCell(7, 6, jg);
+    map.setCell(3, 4, jb);
+    map.setCell(2, 1, jb);
+    map.setCell(5, 0, water);
+    map.setCell(5, 1, water);
+    map.setCell(5, 2, water);
+    map.setCell(5, 3, water);
+    map.setCell(5, 4, water);
+    map.setCell(5, 5, water);
+    map.setCell(5, 6, water);
+    map.setCell(5, 9, tree);
+    map.setCell(3, 9, tree);
+    map.setCell(8, 3, tree);
+    map.setCell(2, 5, tree);
+    map.setCell(1, 4, tree);
+    map.PrintMap();
+    map.FindRobotPosition();
+    Play(Robot, tree, water, jr, jb, jg, map);
+  }
 
-    public static void Play(Robot Robot, Obstacle tree, Obstacle water, Jewell jr, Jewell jb, Jewell jg, Map map)
+  public static void Play(Robot Robot, Obstacle tree, Obstacle water, Jewell jr, Jewell jb, Jewell jg, Map map)
+  {
+    bool running = true;
+    do
     {
-        bool running = true;
-        do
+      Console.WriteLine(Robot.toString());
+      Console.WriteLine("Enter the command: ");
+      string command = Console.ReadLine()!;
+
+      if (command.Equals("quit"))
+      {
+        running = false;
+      }
+      else if (command.Equals("w"))
+      {
+        try
         {
-            Console.WriteLine(Robot.toString());
-            Console.WriteLine("Enter the command: ");
-            string command = Console.ReadLine()!;
-
-            if (command.Equals("quit"))
-            {
-                running = false;
-            }
-            else if (command.Equals("w"))
-            {
-                try
-                {
-                    map.FindRobotPosition();
-                    Robot.moveToTop(map);
-                    map.PrintMap();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-            }
-            else if (command.Equals("a"))
-            {
-                try
-                {
-                    map.FindRobotPosition();
-                    Robot.moveToLeft(map);
-                    map.PrintMap();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-            }
-            else if (command.Equals("s"))
-            {
-                try
-                {
-                    map.FindRobotPosition();
-                    Robot.moveToBottom(map);
-                    map.PrintMap();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-            }
-            else if (command.Equals("d"))
-            {
-                try
-                {
-                    map.FindRobotPosition();
-                    Robot.moveToRight(map);
-                    map.PrintMap();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-            }
-            else if (command.Equals("g"))
-            {
-                try
-                {
-                    map.FindRobotPosition();
-                    Robot.captureItem(map);
-                    map.PrintMap();
-                }
-                catch
-                {
-                    Console.WriteLine("Não existe joia ou arvore ao redor");
-                }
-            }
-        } while (running);
-    }
+          map.FindRobotPosition();
+          Robot.moveToTop(map);
+          map.PrintMap();
+        }
+        catch (Exception e)
+        {
+          Console.WriteLine(e);
+        }
+      }
+      else if (command.Equals("a"))
+      {
+        try
+        {
+          map.FindRobotPosition();
+          Robot.moveToLeft(map);
+          map.PrintMap();
+        }
+        catch (Exception e)
+        {
+          Console.WriteLine(e);
+        }
+      }
+      else if (command.Equals("s"))
+      {
+        try
+        {
+          map.FindRobotPosition();
+          Robot.moveToBottom(map);
+          map.PrintMap();
+        }
+        catch (Exception e)
+        {
+          Console.WriteLine(e);
+        }
+      }
+      else if (command.Equals("d"))
+      {
+        try
+        {
+          map.FindRobotPosition();
+          Robot.moveToRight(map);
+          map.PrintMap();
+        }
+        catch (Exception e)
+        {
+          Console.WriteLine(e);
+        }
+      }
+      else if (command.Equals("g"))
+      {
+        try
+        {
+          map.FindRobotPosition();
+          // Robot.captureItem(map);
+          Robot.captureItem(map);
+          map.PrintMap();
+        }
+        catch
+        {
+          Console.WriteLine("Não existe joia ou arvore ao redor");
+        }
+      }
+    } while (running);
+  }
 }
