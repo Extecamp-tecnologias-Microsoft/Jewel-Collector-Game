ec166c82ce73de988ed1674df4cfefd8fabbf9d6
Gabriel Rodrigues <gabers357@gmail.com>
2023-06-11 20:05:38 -0300

    Finalizada a descricao do das classes do projeto

--- Classes/Jewell.cs
+++ Classes/Jewell.cs
@@ -1,7 +1,6 @@
 namespace JewellNS;
 /// <summary>
-/// A classe Jewell herda todos os atributos da classe cell 
-/// ela fica responsável por contabilizar a pontuação e as Jóias.
+/// Classe responsavel pelas joias.
 /// </summary>
 public class Jewell : Cell
 {
