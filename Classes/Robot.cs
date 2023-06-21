070ba5008573ccdf96829d3571710e90e2f423fb
Eduardo Franco <franc0edu24@gmail.com>
2023-06-20 21:50:08 -0300

    levels method and random itens

--- Classes/Robot.cs
+++ Classes/Robot.cs
@@ -4,26 +4,23 @@ namespace JewellNS;
 /// </summary>
 public class Robot : Cell
 {
+  private Map map;
   private int bagValue = 0;
   private Cell[] bagItems = new Cell[] { };
   private int RobotLine;
   private int RobotColumn;
-  private int numbercapturedItems = 0;
-  private int itemsOnMap = 0;
-  private bool changeMap = false;
   public int getRobotLine() { return this.RobotLine; }
   public int getRobotColumn() { return this.RobotColumn; }
   public void setRobotLine(int line) { this.RobotLine = line; }
   public void setRobotColumn(int column) { this.RobotColumn = column; }
 
-  public string toString() { return $"Bag total items: {this.bagItems.Length} | Bag total value: {this.bagValue} | Energy: {this.LevelEnergy}, {this.numbercapturedItems} {itemsOnMap}"; }
+  public string toString() { return $"Bag total items: {this.bagItems.Length} | Bag total value: {this.bagValue} | Energy: {this.LevelEnergy}"; }
   public void verifyEnergyLevel()
   {
     Console.Clear();
     if (LevelEnergy <= 0)
     {
-      Console.WriteLine("Suas energias acabaram");
-      Environment.Exit(0);
+      throw new Exception();
     }
   }
   public void moveToLeft(Map map)
@@ -84,7 +81,6 @@ public class Robot : Cell
     int playerLine = getRobotLine();
     int columnPlayer = getRobotColumn();
     int captureRange = 1;
-    this.itemsOnMap = map.getNumberItems();
 
     if ((map.getCell(playerLine + captureRange, columnPlayer) is Jewell) ||
         (map.getCell(playerLine + captureRange, columnPlayer) is Obstacle obstacle3) && obstacle3.Symbol == " $$ ")
@@ -113,11 +109,6 @@ public class Robot : Cell
       updateEnergy(map.getCell(playerLine, columnPlayer - captureRange));
       updateBag(map.getCell(playerLine, columnPlayer - captureRange));
       map.removeCell(playerLine, columnPlayer - captureRange);
-    }
-    numbercapturedItems++;
-    if (numbercapturedItems == itemsOnMap)
-    {
-      map.changeMap = true;
     }
   }
   /// <summary>
